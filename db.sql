6923e7bec6e27e81f74faf60d8832ceea407036c
SaurabhNikam <saurabhnikam2711@gmail.com>
2023-05-17 09:32:50 +0530

    test

---
+++ db.sql
@@ -0,0 +1,5 @@
+create table Customer(cid int,cname varchar(20),email varchar(20),password varchar(20),location varchar(20));
+
+
+
+create table travel_agency(tid int,tname varchar(20),payement double(9,2),travel_type varchar(20),bookingdate date );
